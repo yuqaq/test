8efa45da5c2413d04a3921a9e21112451877642b
wy <yu.wang.c@cdcalabar.com>
2018-04-26 16:30:31 +0800

    first commit

---
+++ src/main/java/com/example/demo/JmeterTestApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JmeterTestApplication {
+
+	@RequestMapping("/")
+	String index() {
+		return "Hello Spring Boot";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterTestApplication.class, args);
+	}
+}
